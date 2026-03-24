0e99372ee4811135e77ba6beae45583e9118c404
refael_saidov <refaelsaidov1@gmail.com>
2026-03-24 10:43:49 +0200

    add primary product

---
+++ data/routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, HTTPException
+from services import average_from_front_input
+
+router = APIRouter()
+
+@router.get("/cities-stats")
+def get_cities_stats(date: str = "2026-03-23T09:41:05.623Z", city: str = "חיפה"):
+
+    try:
+        result = average_from_front_input(date, city)
+        return {"status": "ok", "data": result}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
